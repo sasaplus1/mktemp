861f86885cfa34c6e91be772287023b983da7928
sasaplus1 <sasaplus1@gmail.com>
2013-02-06 00:06:37 +0900

    プレースホルダの置換する位置を変更するテストを追加

--- test/test-mkrand.js
+++ test/test-mkrand.js
@@ -6,14 +6,32 @@ suite('mkrandのテスト', function() {
   test('"X"の数だけ0-9a-zA-Zのランダムな文字列に置換されること', function() {
     var randomString = mkrand('XXXXX');
 
-    assert.isTrue(/[\da-zA-Z]{5}/.test(randomString),
+    assert.isTrue(/^[\da-zA-Z]{5}$/.test(randomString),
         'replace to random string from "XXXXX"');
     assert.notStrictEqual(randomString, 'XXXXX',
         'randomString is not "XXXXX"');
   });
 
-  test('"X"が末尾にない場合は置換されないこと', function() {
-    assert.strictEqual(mkrand('XXXXX_'), 'XXXXX_',
+  test('"X"が複数ある場合、末尾の"X"が優先的に置換されること', function() {
+    var randomString = mkrand('XXXXX_XXXXX');
+
+    assert.isTrue(/^X{5}_[\da-zA-Z]{5}$/.test(randomString),
+        'replace to random string from XXXXX');
+    assert.notStrictEqual(randomString, 'XXXXX_XXXXX',
+        'randomString is not "XXXXX_XXXXX"');
+  });
+
+  test('"X"が末尾で無くても置換されること', function() {
+    var randomString = mkrand('temp-XXXXX.tmp');
+
+    assert.isTrue(/^temp-[\da-zA-Z]{5}\.tmp$/.test(randomString),
+        'replace to random string from temp-XXXXX.tmp');
+    assert.notStrictEqual(randomString, 'temp-XXXXX.tmp',
+        'randomString is not "temp-XXXXX.tmp"');
+  });
+
+  test('"X"が無い場合置換されないこと', function() {
+    assert.strictEqual(mkrand('abcdef'), 'abcdef',
         'randomString is not replaced');
   });
 
