2521db58352f3caf360c534f4e7777eed70b4fe7
sasaplus1 <sasaplus1@gmail.com>
2013-06-19 20:31:39 +0900

    changed benchmark
    - appended
    - refactored

--- bench/index.js
+++ bench/index.js
@@ -1,18 +1,23 @@
 var benchmark = require('benchmark'),
     mkrand = require('../lib/mkrand'),
     suite = new benchmark.Suite,
-    str100, str1000000;
+    str8, str100, str1000000;
 
 suite
+  .add('8', {
+    fn: function() {
+      mkrand(str8);
+    },
+    onStart: function() {
+      str8 = Array(8 + 1).join('X');
+    }
+  })
   .add('100', {
     fn: function() {
       mkrand(str100);
     },
     onStart: function() {
       str100 = Array(100 + 1).join('X');
-    },
-    onComplete: function(event) {
-      console.log(String(event.target));
     }
   })
   .add('1,000,000', {
@@ -21,10 +26,10 @@ suite
     },
     onStart: function() {
       str1000000 = Array(1000000 + 1).join('X');
-    },
-    onComplete: function(event) {
-      console.log(String(event.target));
     }
+  })
+  .on('cycle', function(event) {
+    console.log(String(event.target));
   })
   .run({
     async: true
