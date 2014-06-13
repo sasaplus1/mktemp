186da09c861c6778207f0f8a7aa0bb2fd33c67e8
sasaplus1 <sasaplus1@gmail.com>
2014-06-14 01:10:48 +0900

    compacted

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 /*!
- * mktemp Copyright(c) 2013 sasa+1
+ * @license mktemp Copyright(c) 2013-2014 sasa+1
  * https://github.com/sasaplus1/mktemp
  * Released under the MIT license.
  */
 
+var mktemp = require('./lib/mktemp');
 
-/** export mktemp functions. */
-module.exports = require('./lib');
+
+/**
+ * export.
+ */
+module.exports = {
+  createFile: mktemp.createFile,
+  createFileSync: mktemp.createFileSync,
+  createDir: mktemp.createDir,
+  createDirSync: mktemp.createDirSync
+};
