a96ffb221879e16d718e28b36e55185f5b1fd095
sasaplus1 <sasaplus1@gmail.com>
2019-05-28 19:45:59 +0900

    translate to TypeScript from JavaScript

---
+++ src/dir.ts
@@ -0,0 +1,93 @@
+/**
+ * @file for directory processing
+ */
+
+import * as fs from 'fs';
+
+import * as uniqueName from './unique_name';
+
+/**
+ * try create unique name directory
+ *
+ * @param {string} template template string for directory name
+ * @param {number} retryCount
+ * @param {Function} callback
+ */
+function tryCreateDir(
+  template: string,
+  retryCount: number,
+  callback: (err: NodeJS.ErrnoException | null, dirname: string | null) => void
+): void {
+  let dirname: string | null = uniqueName.generate(template);
+
+  fs.mkdir(dirname, 448 /*=0700*/, function(err) {
+    if (err) {
+      if (err.code === 'EEXIST' && retryCount > 0) {
+        setImmediate(tryCreateDir, template, retryCount - 1, callback);
+
+        return;
+      }
+
+      // dirname set to null if throws error
+      dirname = null;
+    }
+
+    callback(err, dirname);
+  });
+}
+
+/**
+ * create unique name directory
+ *
+ * @param {string} template template string for file directory
+ * @param {Function} [callback]
+ * @return {Promise}
+ */
+export function createDir(
+  template: string,
+  callback: (err: NodeJS.ErrnoException | null, dirname: string | null) => void
+): undefined | Promise<string | null> {
+  if (typeof callback !== 'function') {
+    return new Promise(function(resolve, reject) {
+      createDir(template, (err, dirname) =>
+        err ? reject(err) : resolve(dirname)
+      );
+    });
+  }
+
+  const retryCount = uniqueName.getOutcomeCount(template);
+
+  tryCreateDir(template, retryCount, callback);
+}
+
+/**
+ * sync version createDir
+ *
+ * @param {string} template template string for directory name
+ * @throws {Error} if caught error
+ * @return {string} dirname
+ */
+export function createDirSync(template: string): string {
+  let dirname: string;
+  let isExist: boolean;
+
+  let retryCount = uniqueName.getOutcomeCount(template);
+
+  do {
+    isExist = false;
+    retryCount -= 1;
+    dirname = uniqueName.generate(template);
+
+    try {
+      fs.mkdirSync(dirname, 448 /*=0700*/);
+    } catch (e) {
+      if (e.code === 'EEXIST' && retryCount > 0) {
+        isExist = true;
+      } else {
+        throw e;
+      }
+    }
+  } while (isExist);
+
+  return dirname;
+}
---
+++ src/unique_name.ts
@@ -0,0 +1,61 @@
+/**
+ * @file generate unique name
+ */
+
+const placeholder = /(X+)[^X]*$/;
+const table = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const tableLength = table.length;
+
+/**
+ * count the number of possible outcomes for the template
+ *
+ * @param {string} template template string
+ * @throws {TypeError} if template is not a string
+ * @return {number} count of possible outcomes
+ */
+export function getOutcomeCount(template: string): number {
+  if (typeof template !== 'string') {
+    throw new TypeError(`template must be a string: ${template}`);
+  }
+
+  const matches = template.match(placeholder);
+
+  if (matches === null) {
+    return 1;
+  }
+
+  return Math.pow(tableLength, matches[1].length);
+}
+
+/**
+ * generate unique name
+ *
+ * @param {string} template template string
+ * @throws {TypeError} if template is not a string
+ * @return {string} unique name string
+ */
+export function generate(template: string): string {
+  if (typeof template !== 'string') {
+    throw new TypeError(`template must be a string: ${template}`);
+  }
+
+  const matches = template.match(placeholder);
+
+  if (matches === null) {
+    return template;
+  }
+
+  const result = [];
+
+  for (let i = 0, len = matches[1].length; i < len; ++i) {
+    result.push(table[Math.floor(Math.random() * tableLength)]);
+  }
+
+  const { index: matchesIndex = 0 } = matches;
+
+  return (
+    template.slice(0, matchesIndex) +
+    result.join('') +
+    template.slice(matchesIndex + result.length)
+  );
+}
