0993e80b90a26ee7b2ef7958fd3de9162ad91b99
sasaplus1 <sasaplus1@gmail.com>
2022-06-28 00:12:53 +0900

    update sources

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,14 +1,21 @@
-import assert = require('assert');
+import assert from 'assert';
 
-import { describe, it } from 'mocha';
+import * as mktemp from './index';
 
-import * as mktemp from '../';
-
-describe('index', function() {
-  it('should export some functions', function() {
+describe('index', function () {
+  it('should export createFile', function () {
     assert(typeof mktemp.createFile === 'function');
+  });
+  it('should export createFileSync', function () {
     assert(typeof mktemp.createFileSync === 'function');
+  });
+  it('should export createDir', function () {
     assert(typeof mktemp.createDir === 'function');
+  });
+  it('should export createDirSync', function () {
     assert(typeof mktemp.createDirSync === 'function');
+  });
+  it('should export generateUniqueName', function () {
+    assert(typeof mktemp.generateUniqueName === 'function');
   });
 });
