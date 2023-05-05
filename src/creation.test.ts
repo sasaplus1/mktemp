bed2bc123cbf6e05c607c2f763dd4deec9d93b87
sasaplus1 <sasaplus1@gmail.com>
2023-05-05 10:42:06 +0900

    add node: prefix

--- src/creation.test.ts
+++ src/creation.test.ts
@@ -1,5 +1,5 @@
-import assert from 'assert';
-import fs from 'fs';
+import assert from 'node:assert';
+import fs from 'node:fs';
 
 import * as sinon from 'sinon';
 
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,4 @@
-import assert from 'assert';
+import assert from 'node:assert';
 
 import * as mktemp from './index';
 
