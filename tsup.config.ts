617d04cace355349c576f47eb94537f398396bda
sasaplus1 <sasaplus1@gmail.com>
2025-06-15 03:17:06 +0900

    add tsup.config.ts

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+/* eslint-disable node/no-unsupported-features/es-syntax */
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  bundle: false,
+  clean: true,
+  dts: true,
+  entry: ['src/**/*.ts', '!src/**/*.test.ts'],
+  format: ['cjs', 'esm'],
+  sourcemap: true,
+  splitting: false
+});
